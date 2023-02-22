03b7e6381c78949e26190e9eed9c64146ed76214
NB05UNKNOWN <np01cp4s210012@islingtoncollege.edu.np>
2023-02-22 15:59:18 +0545

    control PORT

--- server/server.js
+++ server/server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const core = require('cors');
 
-const port = 5000;
+require('dotenv').config({ path: './config.env' });
+const port = process.env.PORT || 5000;
 
+//use middleware
 app.use(core());
 app.use(express.json());
 
+//using routes
+app.use(require('./routes/route'));
+
 app.listen(port, () => {
-  console.log(`server is running on port: ${port}`);
+  console.log(`server is running on port: http://localhost:${port}`);
 });
