2d4a189001db9e0649c4dd3604435c6ddcf3020d
NB05UNKNOWN <np01cp4s210012@islingtoncollege.edu.np>
2023-03-12 10:37:50 +0545

    input form frontend complete

--- client/src/Components/Source.js
+++ client/src/Components/Source.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import 'boxicons';
+
+const obj = [
+  {
+    name: 'Salary',
+    Color: '#00FF00',
+  },
+];
 
 function Source() {
-  return <div>Source</div>;
+  return (
+    <div className="flex flex-col py-6 gap-3">
+      <h1 className="py-4 font-bold text-xl">Source</h1>
+      {obj.map((v, i) => (
+        <Income key={i} Source={v}></Income>
+      ))}
+    </div>
+  );
+}
+
+function Income({ Source }) {
+  if (!Source) return null;
+  return (
+    <div
+      className="item flex justify-center bg-gray-50 py-2 rounded-r"
+      style={{ borderRight: `8px solid ${Source.color ?? '#00FF00'}` }}
+    >
+      <button className="px-3">
+        <box-icon
+          data-id={Source._id ?? ''}
+          size="15px"
+          color={Source.color ?? '#00FF00'}
+          name="trash"
+        />
+      </button>
+      <span className="block w-full">
+        {Source.name ?? ''}-{'>'} {Source.amount}
+      </span>
+    </div>
+  );
 }
 
 export default Source;
